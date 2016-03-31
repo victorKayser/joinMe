2fca8a34af51e89807970c78ec4de712458ccc86
Victor Kayser <kayser.vic@gmail.com>
2016-03-31 17:19:44 +0200

    watcher géoloc

--- joinMe/www/js/app.js
+++ joinMe/www/js/app.js
@@ -3,71 +3,109 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'ngCordova', 'ngMap'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
 
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
     }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
+    if(window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
+
   $stateProvider
-
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
-
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
+  .state('map', {
+    url: '/',
+    templateUrl: 'templates/map.html',
+    controller: 'MapCtrl'
   });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-});
+
+  $urlRouterProvider.otherwise("/");
+
+})
+
+.controller('MapCtrl', function($scope, $state, NgMap, $cordovaGeolocation) {
+  NgMap.getMap().then(function(map) {
+
+    // check in LS if old position are saved
+    // if yes
+    if (window.localStorage['lastPosition']) {
+      var lastPosition = JSON.parse(window.localStorage['lastPosition'] || '{}');
+      //init the center on the old pos
+      $scope.map.setCenter(
+         new google.maps.LatLng(lastPosition.split(', ')[0], lastPosition.split(', ')[1])
+      );
+    }
+    else {
+      $scope.map.setZoom(11);
+      // init in Paris
+      $scope.map.setCenter(
+         new google.maps.LatLng('48.8575954','2.3609439')
+      );
+    }
+    // get the current position one time
+    $cordovaGeolocation.getCurrentPosition().then(function(position){
+       var lat = position.coords.latitude;
+       var lng = position.coords.longitude;
+
+       // init the center on the position
+       $scope.map.setCenter(
+          new google.maps.LatLng(lat, lng)
+       );
+       // zoom
+       $scope.map.setZoom(15);
+
+       // init the marker
+       $scope.markerLocalisation = new google.maps.Marker({
+          position: {lat: lat, lng: lng},
+          map: map,
+          title: 'test',
+          draggable: true,
+          icon: {
+            url : 'img/marker-user.png',
+            scaledSize: new google.maps.Size(20, 20)
+          }
+        });
+    });
+
+    // start the watcher
+     var watch = $cordovaGeolocation.watchPosition({
+         frequency: 1000,
+         timeout: 3000,
+         enableHighAccuracy: false
+     }).then(function () {
+         }, function (err) {
+             // you need to enhance that point
+
+         }, function (position) {
+           // for each new position
+           var lat = position.coords.latitude;
+           var lng = position.coords.longitude;
+           var positionLS = lat + ', ' + lng;
+           // center the map
+           $scope.map.setCenter(
+              new google.maps.LatLng(lat, lng)
+           );
+
+           // set on LS the new position
+           window.localStorage['lastPosition'] = JSON.stringify(positionLS);
+
+           // modify the position of the marker
+           var newlatlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+           $scope.markerLocalisation.setPosition(newlatlng);
+         }
+     );
+  });
+})
